0cd28ce865d11049cc3abc6157f18abc91382e20
Mariano Andres Botta <marianoabotta@gmail.com>
2020-07-21 16:33:18 -0300

    connection provider

--- libs/images/src/lib/multer-config.service.ts
+++ libs/images/src/lib/multer-config.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { MulterModuleOptions, MulterOptionsFactory } from '@nestjs/platform-express';
 import * as GridFsStorage from 'multer-gridfs-storage';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
 export class GridFsMulterConfigService implements MulterOptionsFactory {
     gridFsStorage: GridFsStorage;
-    constructor() {
+    constructor(@InjectConnection() private readonly connection: Connection) {
         this.gridFsStorage = new GridFsStorage({
             // [TODO] DESHARCODEAAR ESTO
-            url: 'mongodb://localhost/access-control',
+            db: this.connection.db,
             file: (req, file) => {
                 return new Promise((resolve, reject) => {
                     const filename = file.originalname.trim();
